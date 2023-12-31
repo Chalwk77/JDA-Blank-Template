0c59d128da6faec31909909d542edff19616fcd3
Chalwk77 <jericho.crosby227@gmail.com>
2023-12-31 18:46:18 +1300

    ...

--- src/main/java/com/template/listeners/EventListeners.java
+++ src/main/java/com/template/listeners/EventListeners.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2023, PGR2-Bot. Jericho Crosby <jericho.crosby227@gmail.com> */
+/* Copyright (c) 2023, Discord Bot Template. Jericho Crosby <jericho.crosby227@gmail.com> */
 
 package com.template.listeners;
 
@@ -12,11 +12,6 @@ public class EventListeners extends ListenerAdapter {
     public void onGuildReady(@NotNull GuildReadyEvent event) {
         System.out.println("""
                 _______________________________________________
-                 ____   ____ ____  ____
-                |  _ \\ / ___|  _ \\|___
-                | |_) | |  _| |_) | __) |
-                |  __/| |_| |  _ < / __/
-                |_|    \\____|_| \\_|_____|
                 Copyright (c) 2023, Discord Bot Template. Jericho Crosby
                 _______________________________________________""");
     }
